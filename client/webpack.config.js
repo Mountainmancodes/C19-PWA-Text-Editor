34460d7ad08592e7c9a4f77b6ab3602a376c5c14
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-09-21 19:08:46 -0600

    Make adjustments to try and get render working.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,13 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { argv } = require('process');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: argv.mode || 'development',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
